a67e02c087ddd8bb9ca15ecca2b9ff0f18ec5a38
hinha <martinuz.dawan9@gmail.com>
2021-01-29 17:39:10 +0700

    twitter API

--- server/task.py
+++ server/task.py
@@ -10,6 +10,7 @@ from decouple import config
 from celery.signals import after_setup_task_logger
 from celery.app.log import TaskFormatter
 
+from tweet_api.api import TweetSearch
 from orakarik.scrape.tweet_scrape import SnTweetScrape
 from orakarik.scrape.ig_scrape import SnInstagramScraper
 # from . import summary
@@ -52,9 +53,61 @@ app.conf.update(
 logger = logging.getLogger(__name__)
 
 
+# @app.task # Deprecate
+# def twitter_scrape_v1(dataSequence):
+#
+#     task_request = {}
+#     if isinstance(dataSequence, list):
+#         if len(dataSequence) > 0 and len(dataSequence) == 1:
+#             task_request = dataSequence[0]
+#         else:
+#             task_request = dataSequence
+#     else:
+#         raise Exception("length of dataSequence")
+#
+#     if task_request["type"] == "account":
+#         since = datetime.now() - timedelta(days=365)
+#         until = datetime.now()
+#     elif task_request["type"] == "hashtag":
+#         since = datetime.now() - timedelta(days=30)
+#         until = datetime.now()
+#
+#     scrape = SnTweetScrape(since.strftime('%Y-%m-%d'), until.strftime('%Y-%m-%d'), 120, proxy=False, proxy_dict={})
+#     twitter_data = []
+#     if task_request["type"] == "account":
+#         twitter_data = scrape.tweetAccount(task_request["keyword"])
+#     elif task_request["type"] == "hashtag":
+#         twitter_data = scrape.tweetHashtag(task_request["keyword"], lang="id")
+#
+#         df = pd.DataFrame(twitter_data)
+#
+#         oDB = app.conf.get("MYSQL_DATABASE")
+#         scumm = Summary(oDB)
+#         most_egg = scumm.most_engg_user(df)
+#         if most_egg:
+#             for x in most_egg:
+#                 result = scumm.find_user_most_engaged(x, task_request["media"], task_request["id"])
+#                 if not result:
+#                     scumm.insert_most_engaged(x, task_request["media"], task_request["id"])
+#     else:
+#         twitter_data = scrape.tweetSearch(task_request, lang="id")
+#
+#     dataTList = []
+#     for tw in twitter_data:
+#         nextTime = datetime.now() - datetime.fromtimestamp(tw['timestamp'])
+#         tw["str_updated_date"] = timeago.format(nextTime, datetime.now())
+#         dataTList.append(tw)
+#
+#     lsUpdate = ""
+#     if len(dataTList) > 0:
+#         lsUpdate = dataTList[0]["created_at"]
+#
+#     dataTList.sort(key=lambda k: k['timestamp'], reverse=True)
+#     return {"results": twitter_data, "last_update": lsUpdate}
+
 @app.task
 def twitter_scrape_v1(dataSequence):
-
+    since, until = None, None
     task_request = {}
     if isinstance(dataSequence, list):
         if len(dataSequence) > 0 and len(dataSequence) == 1:
@@ -71,25 +124,15 @@ def twitter_scrape_v1(dataSequence):
         since = datetime.now() - timedelta(days=30)
         until = datetime.now()
 
-    scrape = SnTweetScrape(since.strftime('%Y-%m-%d'), until.strftime('%Y-%m-%d'), 120, proxy=False, proxy_dict={})
+    scrape = TweetSearch()
+
     twitter_data = []
     if task_request["type"] == "account":
-        twitter_data = scrape.tweetAccount(task_request["keyword"])
+        twitter_data = scrape.searchAccounts(task_request["keyword"], 40)
     elif task_request["type"] == "hashtag":
-        twitter_data = scrape.tweetHashtag(task_request["keyword"], lang="id")
-
-        df = pd.DataFrame(twitter_data)
-
-        oDB = app.conf.get("MYSQL_DATABASE")
-        scumm = Summary(oDB)
-        most_egg = scumm.most_engg_user(df)
-        if most_egg:
-            for x in most_egg:
-                result = scumm.find_user_most_engaged(x, task_request["media"], task_request["id"])
-                if not result:
-                    scumm.insert_most_engaged(x, task_request["media"], task_request["id"])
+        pass
     else:
-        twitter_data = scrape.tweetSearch(task_request, lang="id")
+        twitter_data = scrape.searchMention(task_request["keyword"], since=since, until=until, count=20)
 
     dataTList = []
     for tw in twitter_data:
@@ -107,7 +150,6 @@ def twitter_scrape_v1(dataSequence):
 
 @app.task
 def instagram_scrape_v1(dataSequence):
-
     task_request = {}
     if isinstance(dataSequence, list):
         if len(dataSequence) > 0 and len(dataSequence) == 1:
@@ -143,6 +185,7 @@ def instagram_scrape_v1(dataSequence):
 
     dataTList.sort(key=lambda k: k['timestamp'], reverse=True)
     return {"results": ig_data, "last_update": lsUpdate}
+
 
 @app.task
 def add_reflect(a, b):
---
+++ server/tweet_api/api.py
@@ -0,0 +1,287 @@
+from __future__ import print_function
+from decouple import config
+
+import datetime
+import os
+import re
+import string
+import time
+
+import redis
+
+from orakarik.scrape.cleansing import cleansing
+from orakarik.scrape.sentistrength import sentistrength
+from . import twitter
+
+
+# Create an Api instance.
+api = twitter.Api(consumer_key=config("TWITTER_CONSUMER_KEY"),
+                  consumer_secret=config("TWITTER_CONSUMER_SECRET"),
+                  access_token_key=config("TWITTER_ACCESS_TOKEN"),
+                  access_token_secret=config("TWITTER_ACCESS_TOKEN_SECRET"))
+
+senti = sentistrength.sentistrength()
+
+client = redis.StrictRedis(host=os.environ.get("REDIS_HOST", "localhost"), port=6379, db=2)
+
+
+# client = redis.StrictRedis("127.0.0.1",port=6379, db=10)
+class TweetSearch(object):
+
+    def searchMention(self, term, since, until, count=5):
+        feeds = []
+        results = api.GetSearch(term=term, since=since, until=until, count=count, locale="in", include_entities=True)
+        statusesV = {}
+        for result in results:
+            try:
+                statusesV = self.__Mentiongettiemline_cache(result.id)
+            except:
+                dateSec = time.mktime(datetime.datetime.strptime(result.created_at, "%a %b %d %X +0000 %Y").timetuple())
+                formatted_date = datetime.datetime.fromtimestamp(dateSec).strftime("%Y-%m-%d %H:%M")
+                created_at_time = str(datetime.datetime.fromtimestamp(dateSec))[11:16]
+                text_norm = self.__remove_url(result.full_text)
+                # replace puctuantion
+                text_norm = self.__remove_punctuation(text_norm)
+                text_clean = cleansing.clean_text_light(text_norm)
+                sentiment = senti.main(text_clean)
+                statusesV = {
+                    "name": result.user.name,
+                    "user_profile_is_verified": result.user.verified,
+                    "user_name": result.user.screen_name,
+                    "full_text_norm": self.__remove_url(result.full_text),
+                    "full_text_clean": text_clean,
+                    "text_sentiment": sentiment,
+                    "created_at_time": created_at_time,
+                    "timestamp": int(dateSec),
+                    "created_at": formatted_date,
+                    "favorite_count": result.favorite_count,
+                    "full_text": result.full_text,
+                    "hashtags": ["#" + x.text for x in result.hashtags if x],
+                    "mentions": ["@" + x.screen_name for x in result.user_mentions if x],
+                    "id": result.id,
+                    "lang": result.lang,
+                    "retweet_count": result.retweet_count,
+                    "permalink": f"https://twitter.com/{result.user.id}/status/{result.id}",
+                    "total_engagement": result.retweet_count + result.favorite_count + result.user.statuses_count,
+
+                    "user_description": result.user.description,
+                    "user_location": result.user.location,
+                    "user_favourites_count": result.user.favourites_count,
+                    "user_followers_count": result.user.followers_count,
+                    "user_friends_count": result.user.friends_count,
+                    "user_geo_enabled": result.user.geo_enabled,
+                    "user_id": result.user.id,
+                    "user_listed_count": result.user.listed_count,
+                    "user_statuses_count": result.user.statuses_count,
+                    "user_url": result.user.url if result.user.url else "",
+                    "profile_image_url_https": result.user.profile_image_url_https,
+                }
+
+                self.__Mentionsettimeline_cache(result.id, statusesV)
+            feeds.append(statusesV)
+
+        return feeds
+
+    def searchAccounts(self, name, limit=5):
+        page = 0
+        count = 0
+        # users = api.GetUsersSearch(term=name, page=1, count=10)
+        users = api.GetUser(screen_name=name)
+        views = {
+            "user_profile_is_verified": users.verified,
+            "name": users.name,
+            "user_name": users.screen_name,
+            "user_description": users.description,
+            "user_location": users.location,
+            "user_favourites_count": users.favourites_count,
+            "user_followers_count": users.followers_count,
+            "user_friends_count": users.friends_count,
+            "user_geo_enabled": users.geo_enabled,
+            "user_id": users.id,
+            "user_listed_count": users.listed_count,
+            "user_statuses_count": users.statuses_count,
+            "user_url": users.url if users.url else "",
+            "user_created_at": users.created_at,
+            "profile_image_url_https": users.profile_image_url_https,
+        }
+        users_timeline = api.GetUserTimeline(user_id=users.id, exclude_replies=True, count=limit + 1)
+
+        feeds = []
+        for tt in users_timeline:
+            fodateSec = time.mktime(datetime.datetime.strptime(tt.created_at, "%a %b %d %X +0000 %Y").timetuple())
+            # print(self.tanggal_start, fodateSec, self.tanggal_end)
+            statusesV = {}
+            count = count + 1
+
+            try:
+                statusesV = self.__gettiemline_cache(tt.id)
+            except Exception as e:
+                count += 1
+                statuses = api.GetStatus(status_id=tt.id)
+
+                dateSec = time.mktime(
+                    datetime.datetime.strptime(statuses.created_at, "%a %b %d %X +0000 %Y").timetuple())
+                formatted_date = datetime.datetime.fromtimestamp(dateSec).strftime("%Y-%m-%d %H:%M")
+                created_at_time = str(datetime.datetime.fromtimestamp(dateSec))[11:16]
+                text_norm = self.__remove_url(statuses.full_text)
+                # replace puctuantion
+                text_norm = self.__remove_punctuation(text_norm)
+                text_clean = cleansing.clean_text_light(text_norm)
+                sentiment = senti.main(text_clean)
+                statusesV = {
+                    "full_text_norm": self.__remove_url(statuses.full_text),
+                    "full_text_clean": text_clean,
+                    "text_sentiment": sentiment,
+                    "created_at_time": created_at_time,
+                    "timestamp": int(dateSec),
+                    "created_at": formatted_date,
+                    "favorite_count": statuses.favorite_count,
+                    "full_text": statuses.full_text,
+                    "hashtags": ["#" + x.text for x in statuses.hashtags if x],
+                    "mentions": ["@" + x.screen_name for x in statuses.user_mentions if x],
+                    "id": statuses.id,
+                    "lang": statuses.lang,
+                    "retweet_count": statuses.retweet_count,
+                    "permalink": f"https://twitter.com/{statuses.user.id}/status/{statuses.id}",
+                    "total_engagement": statuses.retweet_count + statuses.favorite_count + statuses.user.statuses_count
+                }
+                statusesV.update(views)
+
+                self.__settimeline_cache(tt.id, statusesV)
+
+            feeds.append(statusesV)
+
+            if page % 25 == 0:
+                time.sleep(1)
+
+            if count >= limit != 0:
+                break
+
+        return feeds
+
+    def __Mentiongettiemline_cache(self, key) -> dict:
+        data = client.get(f"mention:{key}")
+        results = data.decode("utf-8")
+
+        return eval(results)
+
+    def __Mentionsettimeline_cache(self, key, raw: dict, expired=86400) -> None:
+        client.set(f"mention:{key}", str(raw), ex=expired)
+
+    def __gettiemline_cache(self, key) -> dict:
+        pprint = f"scrape:twitter_statuses:{key}"
+        data = client.get(pprint)
+        results = data.decode("utf-8")
+
+        return eval(results)
+
+    def __settimeline_cache(self, key, raw: dict, expired=86400) -> None:
+        pprint = f"scrape:twitter_statuses:{key}"
+        client.set(pprint, str(raw), ex=expired)
+
+    def __remove_punctuation(self, s):  # punctuation
+        for c in string.punctuation:
+            s = s.replace(c, " ")
+
+        # replace phone number
+        comp_number = re.compile(
+            "\d?(\(?\d{3}\D{0,3}\d{3}\D{0,3}\d{4})", re.S)
+        text = comp_number.sub(
+            lambda m: re.sub('\d', ' ', m.group(1)), s
+        )
+        text = self.__concate_duplicate(text.lower())
+        return text
+
+    def __remove_url(self, text):
+        # remove all url
+        text = re.sub(r" ?(f|ht)(tp)(s?)(://)(.*)[.|/](.*)", "", text)
+
+        # remove email
+        text = re.sub(r"[\w]+@[\w]+\.[c][o][m]", "", text)
+        # remove text twit
+        text = re.sub(r'((pic\.[^\s]+)|(twitter))', '', text)
+        # remove mentions, hashtag and web
+        text = re.sub(r"(?:\@|#|http?\://)\S+", "", text)
+        # remove url
+        text = re.sub(
+            r'((www\.[^\s]+)|(https?://[^\s]+))', '', text)
+        text = re.sub(r'((https?://[^\s]+))', '', text)
+        text = re.sub(r"(pic[^\s]+)|[\w]+\.[c][o][m]", "", text)
+        # replace non ascii
+        text = re.sub(r'[^\x00-\x7F]+', ' ', text)
+
+        # Remove additional white spaces
+        text = re.sub('[\s]+', ' ', text)
+        text = re.sub('[\n]+', ' ', text)
+
+        return text
+
+    def __remove_emojis(self, data):
+        emoj = re.compile("["
+                          u"\U0001F600-\U0001F64F"  # emoticons
+                          u"\U0001F300-\U0001F5FF"  # symbols & pictographs
+                          u"\U0001F680-\U0001F6FF"  # transport & map symbols
+                          u"\U0001F1E0-\U0001F1FF"  # flags (iOS)
+                          u"\U00002500-\U00002BEF"  # chinese char
+                          u"\U00002702-\U000027B0"
+                          u"\U00002702-\U000027B0"
+                          u"\U000024C2-\U0001F251"
+                          u"\U0001f926-\U0001f937"
+                          u"\U00010000-\U0010ffff"
+                          u"\u2640-\u2642"
+                          u"\u2600-\u2B55"
+                          u"\u200d"
+                          u"\u23cf"
+                          u"\u23e9"
+                          u"\u231a"
+                          u"\ufe0f"  # dingbats
+                          u"\u3030"
+                          "]+", re.UNICODE)
+        return re.sub(emoj, '', data)
+
+    def __concate_duplicate(self, text):
+        te = "a" + r"{3}"
+        rep = re.sub(te, " 3", text)
+        te = "i" + r"{3}"
+        rep = re.sub(te, " 3", rep)
+        te = "u" + r"{3}"
+        rep = re.sub(te, " 3", rep)
+        te = "e" + r"{3}"
+        rep = re.sub(te, " 3", rep)
+        te = "o" + r"{3}"
+        rep = re.sub(te, " 3", rep)
+
+        # Extends
+        te = "c" + r"{3}"
+        rep = re.sub(te, " 3", rep)
+        te = "k" + r"{3}"
+        rep = re.sub(te, " 3", rep)
+        te = "w" + r"{3}"
+        rep = re.sub(te, " 3", rep)
+        te = "h" + r"{3}"
+        rep = re.sub(te, " 3", rep)
+
+        return rep
+
+
+def check_user(name):
+    try:
+        users = api.GetUser(screen_name=name)
+        if users:
+            if users.protected is True:
+                return {'status': False, 'is_private': 1}
+            return {'status': True, 'is_private': 0, "id": users.id}
+        else:
+            return {'status': False, 'is_private': 0, "id": "00"}
+    except Exception as e:
+        print(e)
+        return {'status': False, 'is_private': 0, "id": "00"}
+
+
+def check_profile(name):
+    try:
+        users = api.GetUser(screen_name=name)
+        return users
+    except Exception as e:
+        print(e)
+        return False
